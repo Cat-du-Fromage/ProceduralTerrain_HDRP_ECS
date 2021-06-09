39cc7e46a3803f1b6959dc1f32045ea69cdaa16e
Florian <florian.duruz@gmail.com>
2021-06-09 22:26:07 +0200

    begin IJobParallelFor PerlinNoise

--- Assets/ECSScript/ProceduralGeneration/Jobs/HeightMapJobs.cs
+++ Assets/ECSScript/ProceduralGeneration/Jobs/HeightMapJobs.cs
@@ -25,6 +25,7 @@ namespace KaizerwaldCode.ProceduralGeneration.Jobs
 
         public void Execute()
         {
+            // 1 Process octavesOffset and fill it; => separate process(so we can use it as a read only)
             #region Random
             Unity.Mathematics.Random pRNG = new Unity.Mathematics.Random((uint) seedJob);
 
@@ -49,6 +50,7 @@ namespace KaizerwaldCode.ProceduralGeneration.Jobs
                     float amplitude = 1;
                     float frequency = 1;
                     float noiseHeight = 0;
+                    //Not needed in parallel! it's a layering of noise so it must be done contigiously
                     for (int i = 0; i < octavesJob; i++)
                     {
                         float sampleX = math.mul((x - halfMapSize) / scaleJob, frequency) + octOffsetArray[i].x;
@@ -57,7 +59,8 @@ namespace KaizerwaldCode.ProceduralGeneration.Jobs
 
                         float pNoiseValue = snoise(sampleXY);
                         noiseHeight = math.mad(pNoiseValue, amplitude, noiseHeight);
-                        //amplitude : decrease each octaves; frequency : increase each octaves
+                        //lacunarity: controls increase in frequency of octaves
+                        //Persistance : controls decrease in amplitude of octaves
                         amplitude = math.mul(amplitude, persistanceJob);
                         frequency = math.mul(frequency, lacunarityJob);
                     }
@@ -86,4 +89,80 @@ namespace KaizerwaldCode.ProceduralGeneration.Jobs
             }
         }
     }
+    /*
+     List<jobhandles> JobList = new List<JobHandles>()
+    noiseMap Nativearray
+        for (int y = 0; y < mapSizeJob; y++)
+            {
+                for (int x = 0; x < mapSizeJob; x++)
+                {
+                    if(x== 0 && y == 0)
+                    {
+                        var new PerlinJob
+                        {
+                            x,
+                            y,
+                        }
+                        JobList.add(job.schedule(noiseMap.Length, 250))
+                    }
+                    list<Job>.add(job.schedule(noiseMap.Length, 250,jobhandles[i-1]))
+                }
+            }
+    */
+    [BurstCompile(CompileSynchronously = true)]
+    public struct NewPerlinJob : IJobParallelFor
+    {
+        [ReadOnly] public int MapSizeJob;
+        [ReadOnly] public int OctavesJob;
+        [ReadOnly] public float LacunarityJob;
+        [ReadOnly] public float PersistanceJob;
+        [ReadOnly] public float ScaleJob;
+
+        [ReadOnly]NativeArray<float2> octOffsetArray;
+        public NativeArray<float> noiseMap;
+        public void Execute(int index)
+        {
+            //FOR LOOP
+            float maxNoiseHeight = float.MinValue;
+            float minNoiseHeight = float.MaxValue;
+
+            float halfMapSize = MapSizeJob / 2f;
+
+            int y = (int)math.floor(index / MapSizeJob);
+            int x = index - math.mul(y, MapSizeJob);
+
+            float amplitude = 1;
+            float frequency = 1;
+            float noiseHeight = 0;
+            //Not needed in parallel! it's a layering of noise so it must be done contigiously
+            for (int i = 0; i < OctavesJob; i++)
+            {
+                float sampleX = math.mul((x - halfMapSize) / ScaleJob, frequency) + octOffsetArray[i].x;
+                float sampleY = math.mul((y - halfMapSize) / ScaleJob, frequency) + octOffsetArray[i].y;
+                float2 sampleXY = new float2(sampleX, sampleY);
+
+                float pNoiseValue = snoise(sampleXY);
+                noiseHeight = math.mad(pNoiseValue, amplitude, noiseHeight);
+                //lacunarity: controls increase in frequency of octaves
+                //Persistance : controls decrease in amplitude of octaves
+                amplitude = math.mul(amplitude, PersistanceJob);
+                frequency = math.mul(frequency, LacunarityJob);
+            }
+
+            if (noiseHeight > maxNoiseHeight)
+            {
+                maxNoiseHeight = noiseHeight;
+            }
+            else if (noiseHeight < minNoiseHeight)
+            {
+                minNoiseHeight = noiseHeight;
+            }
+            noiseMap[index] = noiseHeight;
+            //noiseMap[math.mad(y, MapSizeJob, x)] = noiseHeight;
+        }
+    }
+    //.Schedule(Data.Length, DEFAULT_BATCH_COUNT, dependency)
+
+    //.Schedule(OctavesOffset.Length, DEFAULT_BATCH_COUNT, dependency)
+    //.Schedule(NoiseMap.Length, DEFAULT_BATCH_COUNT, dependency)
 }
